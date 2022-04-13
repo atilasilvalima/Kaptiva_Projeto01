7499397b02656014baefbac8fe9cf7d358251497
Átila da Silva Lima <atilasilvalima@gmail.com>
2022-04-12 23:07:50 -0300

    Ajuste Header e Footer

---
+++ layouts/footer.php
@@ -0,0 +1,9 @@
+<!-- Footer -->
+<footer class="sticky-footer bg-white Rodape">
+    <div class="container my-auto">
+        <div class="copyright text-center my-auto">
+            <span>Copyright © 2022 Kaptiva. All Rights Reserved.</span>
+        </div>
+    </div>
+</footer>
+<!-- End of Footer -->
